5b13d95792187ec1248b7f12a283888cac3105e5
3mushrooms <tamimi@Ahmads-MacBook-Pro.local>
2016-08-21 22:45:03 -0400

    adding Search support to users_route.js by adding searches resource

--- users/user_routes.js
+++ users/user_routes.js
@@ -2,7 +2,6 @@ var _ = require('lodash')
 var User = require('../users/user_model.js')
 
 module.exports = function(app) {
-        
     //create
     app.post('/users', function(req, res) {
         //validate request body before processing it; if not valid issue status: 400 - Bad Request
@@ -28,6 +27,7 @@ module.exports = function(app) {
                     // user found!  do not allow duplicate.
                     console.log('error 409 - cannot create user.  user already exist');
                     res.status(409).json({info: 'cannot create user.  user already exist', data: user});
+                    return;
                 } else {     
                     var newUser = new User(req.body);
                     newUser.save(function(err){
@@ -37,12 +37,45 @@ module.exports = function(app) {
                             return;
                         };
                         res.status(201).json({info: 'user created successfully'});
+                        return;
                     });
                 }
             });
         }
     });
     
+    //search users.  
+    //Using searches as a resource and applying a POST request to search against searches resource.
+    app.post('/users/searches', function(req, res) {
+        //validate req.body. if not valid issue status code 400 bad request
+        req.checkBody('username', 'please enter a name to search users').notEmpty();
+
+        var errors = req.validationErrors();
+        if(errors) {
+            console.log(errors);
+            res.status(400).json({info: 'cannot complete search process', error: errors});
+            return;
+        } else {
+            User.find({username: new RegExp(req.body.username, 'i')}, function (err, users) { 
+                if(err) {
+                    // error
+                    console.log(err);
+                    res.status(500).json({info: 'error during find user', error: err});
+                    return;
+                };
+                if(users) {
+                    // success
+                    res.status(200).json({info: 'search results for: ' + req.body.username, data: users});
+                    return;
+                } else {
+                    // No content found
+                    res.status(204).json({info: 'user not found'});
+                    return;
+                }
+            });
+        }
+    });
+
     //read
     app.get('/users', function(req, res) {
         User.find(function(err, users) {
@@ -52,6 +85,7 @@ module.exports = function(app) {
                 return;
             };
             res.status(200).json({info: 'users found successfully', data: users});
+            return;
         });
     });
     
@@ -67,12 +101,16 @@ module.exports = function(app) {
             if(user) {
                 // success
                 res.status(200).json({info: 'user found successfully', data: user});
+                return;
             } else {
                 // No content found
                 res.status(204).json({info: 'user not found'});
+                return;
             }
         });
     });
+
+    
     
     //update
     app.put('/users/:id', function(req, res) {
@@ -86,6 +124,7 @@ module.exports = function(app) {
         if(errors) {
             //req.body payload is faulty.  Display error to end user
             res.status(400).json({info: 'cannot create user', error: errors});
+            return;
         } else {
             User.findOne({username: req.params.id}, function (err, user) {
                 if(err) {
@@ -103,10 +142,12 @@ module.exports = function(app) {
                         };
                         // success and user updated successfully
                         res.status(200).json({info: 'user updated successfully'});
+                        return;
                     });  
                 } else {
                     //operation successful but no user found!
                     res.status(204).json({info: 'no user found'});
+                    return;
                 }
             });
         }
@@ -114,8 +155,6 @@ module.exports = function(app) {
     
     //delete
     app.delete('/users/:id', function(req, res) {
-        //TODO:  validate req.body. if not valid issue status code 400 bad request
-        //User.findByIdAndRemove(req.params.id, function (err) {
         User.findOne({username: req.params.id}, function (err, user) {    
             if(err) {
                 //error removing user
@@ -133,9 +172,11 @@ module.exports = function(app) {
                     };
                     // success and user updated successfully
                     res.status(200).json({info: 'user deleted successfully'});
+                    return;
                 }); 
             } else {
                 res.status(204).json({info: 'No user found to delete'});
+                return;
             }
         });
     });
@@ -145,5 +186,4 @@ module.exports = function(app) {
         res.status(200).json({pong: Date.now()})
     });
 
-    
 };
